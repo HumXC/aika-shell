17e59e6e67cd202f7c69769cf6244d5b14cfa8b4
HumXC <Hum-XC@outlook.com>
2024-11-23 10:48:12 +0800

    sync

--- widget/DevicesIcon.tsx
+++ widget/DevicesIcon.tsx
@@ -2,13 +2,13 @@ import { setHoverClassName } from "../utils";
 import BrightnessIcon from "./BrightnessIcon";
 import NetworkIcon from "./NetworkIcon";
 import FloatMenu from "./FloatingMenu";
+import { Gtk } from "astal/gtk3";
 export default function DevicesIcon({ size }: { size: number }) {
     return (
         <eventbox
             setup={(self) => setHoverClassName(self, "DevicesIcon")}
             onClick={(self, e) => {
                 const w = FloatMenu(self);
-                print(w.get_allocation().x);
             }}
         >
             <box
--- widget/FloatingMenu.tsx
+++ widget/FloatingMenu.tsx
@@ -1,18 +1,22 @@
 import { Astal, Gdk, Gtk, Widget } from "astal/gtk3";
+import { RegularWindow } from "./base";
 
 export default function FloatMenu(trigger: Gtk.Widget) {
     return (
         <window
             setup={(self) => {
-                const lo = trigger.get_allocation();
-                self.move(1, 1);
-                print(self.get_allocation().x);
+                print(self.is_floating());
+                self.move(100, 100);
+                self.set;
             }}
+            decorated={false}
+            resizable={true}
             heightRequest={200}
             widthRequest={200}
             title="FloatingMenu"
-            className={"FloatingMenu"}
             keymode={Astal.Keymode.ON_DEMAND}
+            anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.LEFT}
+            className={"FloatingMenu"}
             onKeyPressEvent={(self, e) => {
                 if (e.get_keyval()[1] === Gdk.KEY_Escape) {
                     self.close();
