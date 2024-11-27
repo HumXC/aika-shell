fed065a1f742bc040063f75ca11f82a0f15b2f9f
HumXC <Hum-XC@outlook.com>
2024-11-27 23:29:37 +0800

    1

--- widget/clipboard.tsx
+++ widget/clipboard.tsx
@@ -17,43 +17,41 @@ export default function Clipboard() {
                     self.close();
                 }
             }}
-            setup={(self) => {
-                self.connect("size-allocate", (self) => {
-                    if (init) return;
-                    init = true;
-                    idle(async () => {
-                        let clipboard: Hyprland.Client | undefined = undefined;
-                        while (clipboard === undefined) {
-                            [clipboard] = hypr.clients.filter((c) => {
-                                return c.title === title;
-                            });
-                            await sleep(1);
-                        }
-                        let x = hypr.cursorPosition.x - clipboard.x;
-                        let y = hypr.cursorPosition.y - clipboard.y;
-                        const monitor = hypr.get_focused_monitor();
-                        const gapsOption: {
-                            option: string;
-                            custom: string;
-                            set: boolean;
-                        } = JSON.parse(exec(["hyprctl", "-j", "getoption", "general:gaps_out"]));
-                        let gapx = 0;
-                        let gapy = 0;
-                        if (gapsOption.set) {
-                            const gaps = gapsOption.custom.split(" ").map(Number);
-                            gapx = gaps[0];
-                            gapy = gaps[2];
-                        }
-                        const mWidth = monitor.get_width() / monitor.get_scale();
-                        const mHeight = monitor.get_height() / monitor.get_scale();
-                        if (x + clipboard.x + clipboard.width > mWidth) {
-                            x = mWidth - clipboard.width - clipboard.x - gapx;
-                        }
-                        if (y + clipboard.y + clipboard.height > mHeight) {
-                            y = mHeight - clipboard.height - clipboard.y - gapy;
-                        }
-                        hypr.dispatch("movewindowpixel", `${x} ${y},title:${title}`);
-                    });
+            onDraw={(self) => {
+                if (init) return;
+                init = true;
+                idle(async () => {
+                    let clipboard: Hyprland.Client | undefined = undefined;
+                    while (clipboard === undefined) {
+                        [clipboard] = hypr.clients.filter((c) => {
+                            return c.title === title;
+                        });
+                        await sleep(1);
+                    }
+                    let x = hypr.cursorPosition.x - clipboard.x;
+                    let y = hypr.cursorPosition.y - clipboard.y;
+                    const monitor = hypr.get_focused_monitor();
+                    const gapsOption: {
+                        option: string;
+                        custom: string;
+                        set: boolean;
+                    } = JSON.parse(exec(["hyprctl", "-j", "getoption", "general:gaps_out"]));
+                    let gapx = 0;
+                    let gapy = 0;
+                    if (gapsOption.set) {
+                        const gaps = gapsOption.custom.split(" ").map(Number);
+                        gapx = gaps[0];
+                        gapy = gaps[2];
+                    }
+                    const mWidth = monitor.get_width() / monitor.get_scale();
+                    const mHeight = monitor.get_height() / monitor.get_scale();
+                    if (x + clipboard.x + clipboard.width > mWidth) {
+                        x = mWidth - clipboard.width - clipboard.x - gapx;
+                    }
+                    if (y + clipboard.y + clipboard.height > mHeight) {
+                        y = mHeight - clipboard.height - clipboard.y - gapy;
+                    }
+                    hypr.dispatch("movewindowpixel", `${x} ${y},title:${title}`);
                 });
             }}
         ></RegularWindow>
