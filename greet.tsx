277f916d1cf78f445c068e896c088451e98cad24
HumXC <Hum-XC@outlook.com>
2024-12-12 12:56:39 +0800

    fix

--- greet.tsx
+++ greet.tsx
@@ -118,19 +118,25 @@ function Greeter(monitor: number, main: boolean = true, wallpapers: string[]) {
                             }
                         } else {
                             isAuth.set(true);
-                            Greet.login(session[0], entry.get_text(), session[1], (_, res) => {
-                                try {
-                                    Greet.login_finish(res);
-                                    isDone.set(true);
-                                    timeout(dration, () => App.quit());
-                                } catch (e: any) {
-                                    err.set_text(e.message);
-                                    entry.grab_focus();
-                                    entry.select_region(0, -1);
-                                } finally {
-                                    isAuth.set(false);
-                                }
-                            });
+                            try {
+                                Greet.login(session[0], entry.get_text(), session[1], (_, res) => {
+                                    try {
+                                        Greet.login_finish(res);
+                                    } catch (e: any) {
+                                        err.set_text(e.message);
+                                        entry.grab_focus();
+                                        entry.select_region(0, -1);
+                                    } finally {
+                                        isAuth.set(false);
+                                    }
+                                });
+                                isDone.set(true);
+                                timeout(dration, () => App.quit());
+                            } catch (e: any) {
+                                err.set_text(e.message);
+                                entry.grab_focus();
+                                entry.select_region(0, -1);
+                            }
                         }
                     }
                 }
