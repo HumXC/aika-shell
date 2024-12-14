109ffcc7874a349e88a83351edd809888d2f14f8
HumXC <Hum-XC@outlook.com>
2024-12-14 21:45:09 +0800

    fix

--- nix/aika-greet-hyprland.nix
+++ nix/aika-greet-hyprland.nix
@@ -22,7 +22,7 @@ let
     )
   ;
   hyprConfFinal = pkgs.writeText "aika-greet-hyprland-conf" ''
-    exec-once = ${aika-greet}/bin/aika-greet ${argv} > /home/greeter/aika-greet.log 2>&1; ${pkgs.hyprland}/bin/hyprctl dispatch exit
+    exec-once = ${aika-greet}/bin/aika-greet ${argv} >> /home/greeter/aika-greet.log 2>&1; ${pkgs.hyprland}/bin/hyprctl dispatch exit
     ${hyprConf}
   '';
 in
